86da1c545bdd31b7c534c57172722a04ef481b79
gogy4 <younggogy@gmail.com>
2026-05-06 23:54:26 +0500

    refactor: configure swagger gen

--- ShopAI.Presentation/Program.cs
+++ ShopAI.Presentation/Program.cs
@@ -1,7 +1,9 @@
+using System.Reflection;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ShopAI.Application.DependencyInjection;
 using ShopAI.Infrastructure.DependencyInjection;
 
@@ -12,6 +14,48 @@ builder.Configuration.AddEnvironmentVariables();
 var services = builder.Services;
 
 services.AddControllers();
+
+services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Deputy API",
+        Version = "v1",
+        Description = "API для депутатского приложения"
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Введите ваш JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    });
+
+    options.UseInlineDefinitionsForEnums();
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath)) options.IncludeXmlComments(xmlPath);
+});
+
+services.AddSwaggerGenNewtonsoftSupport();
 
 services
     .AddEndpointsApiExplorer()
