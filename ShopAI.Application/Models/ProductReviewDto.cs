4a09b3c73e517bc4ec88c19ba5cb7628f928546e
Никита <n.belikov@modulbank.ru>
2026-06-04 16:00:55 +0500

    fix product review

--- ShopAI.Application/Models/ProductReviewDto.cs
+++ ShopAI.Application/Models/ProductReviewDto.cs
@@ -1,11 +1,13 @@
 ﻿namespace ShopAI.Application.Models
 {
-    public record ProductReviewDto(
-        Guid Id = default,
-        Guid UserId = default,
-        string UserName = default,
-        List<string>? ImagePaths = null,
-        int Rating = 0,
-        string Comment = "",
-        DateTime CreatedAtUtc = default);
+    public class ProductReviewDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string? UserName { get; set; }
+        public List<string>? ImagePaths { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; } = "";
+        public DateTime CreatedAtUtc { get; set; }
+    }
 }
